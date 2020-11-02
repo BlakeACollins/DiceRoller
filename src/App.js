d5fd61e2a2dfadae4650dc3729d822e540589837
Blake Collins <blakeac369@gmail.com>
2020-11-02 14:55:50 -0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import Die from './Die.js';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Die face="five"/>
+   
+    </div>
+  );
+}
+
+export default App;
